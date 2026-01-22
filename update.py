fb57f18e99b5b4009edb35e939e90eec6e8224b6
northpick <152510041+northpick@users.noreply.github.com>
2026-01-22 18:23:40 +0800

    Delete update.py

--- update.py
+++
@@ -1,6 +0,0 @@
-# 更新依赖.py  —— 运行不了主程序时，双击这个就行
-import os, sys
-print("正在更新 huggingface_hub（只需要点一次）...")
-os.system(f'"{sys.executable}" -m pip install --upgrade huggingface_hub')
-print("\n更新完成！现在可以正常使用主程序了")
-input("按回车关闭...")
